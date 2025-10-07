4529cbe616906cd9f4ad48e6b4c437d5f4d467ef
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-07 13:19:19 -0600

    feat: add validations to Post migration file

--- db/migrate/20251007191410_create_posts.rb
+++ db/migrate/20251007191410_create_posts.rb
@@ -4,8 +4,8 @@ class CreatePosts < ActiveRecord::Migration[8.0]
       t.references :user, null: false, foreign_key: true
       t.text :link
       t.text :body
-      t.integer :upvote
-      t.integer :downvote
+      t.integer :upvote, default: 0
+      t.integer :downvote, default: 0
 
       t.timestamps
     end
