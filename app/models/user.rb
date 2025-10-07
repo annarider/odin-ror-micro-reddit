74ed3c0691f20d9556af58158144ffd60e829ea5
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-07 13:25:26 -0600

    feat: add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 4, maximum: 20 },
+                       format: { with: /\A[A-Za-z0-9_]+\z/,
+                                message: "only allows letters, numbers, and underscores" }
+
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+ validates :password, length: { minimum: 6, maximum: 30 }, if: :password_digest_changed?
+
 end
