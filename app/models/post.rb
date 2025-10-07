289ae2b40aadb73dcb4db91eb62097e0339233e7
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-07 13:28:46 -0600

    feat: add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validate :link_or_body_present
+  validates :link, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+                             message: "must be valid URL" },
+                   allow_blank: true
+
+  private
+
+  def link_or_body_present
+    if link.blank? && body.blank?
+      errors.add(:base, 'Post must have either a valid link or body text')
+    end
+  end
 end
